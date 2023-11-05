f88c908b895c1a5206fd908516c3c6d27b9d809e
Андрей Бровкин <a.brovkin4@yandex.ru>
2023-11-05 23:22:22 +0300

    Code Style: practic 2/2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Функция расчёта наносимого урона.
+
+    В зависимости от класса персонажа функция возвращет
+    значение рассчитанного урона,
+    который сможет нанести персонаж.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +24,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция расчёта заблокированного урона.
+
+    В зависимости от класса персонажа функция возвращет
+    значение рассчитанного урона,
+    который сможет заблокировать персонаж.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +40,11 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция расчёта специального умения.
+
+    В зависимости от класса персонажа функция возвращет тип
+    и значение специального умения.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +56,11 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция старта тренировки.
+
+    В данной функции выполняются проверки атак, блокирования и
+    специального умения для выбранного класса персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +86,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса персонажа.
+
+    Функция позволяет выбрать один из трёх классов персонажа
+    с дальнейшим подтверждением выбора.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,7 +112,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +123,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
